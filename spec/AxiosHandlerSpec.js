07da9ef15aac96461125d296e626a08b4dc3f68e
braddyH <yingxu.he1998@gmail.com>
2020-02-01 02:01:30 +0800

    add axios tests

--- spec/AxiosHandlerSpec.js
+++ spec/AxiosHandlerSpec.js
@@ -10,4 +10,30 @@ describe("Axios", function() {
     console.log(axiosHandler.user_id);
     expect(axiosHandler.user_id).toEqual(28558142);
   });
+
+  // it("should be able to send post request", async function() {
+  //   await axiosHandler.post_request();
+  //   var data = {
+  //     id: 11,
+  //     first_name: "Yingxu",
+  //     last_name: "He",
+  //     email: "yingxu.he1998@gmail.com"
+  //   };
+  //   console.log(axiosHandler.post_data);
+  //   expect(axiosHandler.post_data).toEqual(data);
+  // });
+  //
+  // it("should be able to send delete request", async function() {
+  //   await axiosHandler.delete_request();
+  //   expect(axiosHandler.delete_status).toEqual(200);
+  // });
+
+  it("should be able to send multiple get requests", async function() {
+    await axiosHandler.multiple_get_requests();
+    var data = {
+      yingxu: 28558142,
+      quodai: 44568184
+    };
+    expect(axiosHandler.multiple_user_ids).toEqual(data);
+  });
 });
--- src/AxiosHandler.js
+++ src/AxiosHandler.js
@@ -6,3 +6,34 @@ AxiosHandler.prototype.get_request = async function() {
   var githubInfo = await githubUser.data;
   this.user_id = githubInfo.id;
 }
+
+AxiosHandler.prototype.post_request = async function() {
+    var data = {
+      id: 11,
+      first_name: "Yingxu",
+      last_name: "He",
+      email: "yingxu.he1998@gmail.com"
+    }
+
+    var post_info = await axios.post(`http://localhost:3000/users`, data);
+    var post_data = await post_info.data;
+    this.post_data = post_data;
+}
+
+AxiosHandler.prototype.delete_request = async function() {
+  var delete_info = await axios.delete('http://localhost:3000/users/16/');
+  var delete_status = await delete_info.status;
+  this.delete_status = delete_status;
+}
+
+AxiosHandler.prototype.multiple_get_requests = async function() {
+  let [u1, u2] = await Promise.all([
+    axios.get("https://api.github.com/users/YingxuH"),
+    axios.get("https://api.github.com/users/QuodAI")
+  ])
+
+  this.multiple_user_ids = {
+    yingxu: u1.data.id,
+    quodai: u2.data.id
+  }
+}
