e1965c64d36a64f2e1432d923f19c774183e8b74
braddyH <yingxu.he1998@gmail.com>
2020-02-01 10:42:01 +0800

    add request tests

--- spec/RequestHandlerSpec.js
+++ spec/RequestHandlerSpec.js
@@ -7,6 +7,52 @@ describe("RequestHandler", function() {
   });
 
   it("should be able to send get request", function() {
-    requestHandler.get_request();
+    var oReq = new XMLHttpRequest();
+
+    oReq.open("GET", "https://api.github.com/users/YingxuH");
+    oReq.responseType = "json";
+    oReq.send();
+
+    oReq.onload = function() {
+      if (oReq.status == 200) {
+        expect(oReq.status).toEqual(200);
+        expect(oReq.response.id).toEqual(28558142);
+        this.user_id = oReq.response.id;
+      }
+    };
+  });
+
+  it("should be able to send post request", async function() {
+    var oReq = new XMLHttpRequest();
+    var data = {
+      id: 47,
+      first_name: "Yingxu",
+      last_name: "He",
+      email: "yingxu.he1998@gmail.com"
+    };
+
+    oReq.open("POST", "http://localhost:3000/users");
+    oReq.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    await oReq.send(JSON.stringify(data));
+    oReq.onload = function() {
+      console.log(oReq.response);
+      expect(oReq.status).toEqual(200);
+    }
+  });
+
+  it("should be able to abort a request", function() {
+    var oReq = new XMLHttpRequest();
+    var data = {
+      id: 43,
+      first_name: "Yingxu",
+      last_name: "He",
+      email: "yingxu.he1998@gmail.com"
+    };
+
+    oReq.open("POST", "http://localhost:3000/users");
+    oReq.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    oReq.send(JSON.stringify(data));
+    oReq.abort();
+    expect(oReq.status).toEqual(0);
   });
 });
--- src/RequestHandler.js
+++ src/RequestHandler.js
@@ -1,8 +1,2 @@
 function RequestHandler() {
 }
-
-RequestHandler.prototype.get_request = function() {
-  var oReq = new XMLHttpRequest();
-  oReq.open("GET", "https://api.github.com/users/YingxuH");
-  oReq.send();
-}
